c785dc9eae97b140e5fe494fe3a1723c7b4a709f
khav9 <khav.saroeun@gmail.com>
2026-02-15 10:58:46 +0700

    add lombok for modeling

--- src/main/java/com/example/PosBit/model/Category.java
+++ src/main/java/com/example/PosBit/model/Category.java
@@ -1,60 +1,106 @@
+//package com.example.PosBit.model;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+//import jakarta.persistence.Table;
+//
+//import java.util.List;
+//
+//@Entity
+//@Table(name="category")
+//public class Category {
+//
+//    @Id
+//    private String categoryId;
+//    private String categoryName;
+//
+//    private String categoryDescription;
+//
+//    private String status;
+//
+//    @OneToMany(mappedBy = "category")
+//    @JsonIgnore
+//    private List<Product> products;
+//
+//    public Category() {}
+//
+//    public Category(String categoryId, String categoryName, String categoryDescription, String status) {
+//        super();
+//        this.categoryId = categoryId;
+//        this.categoryName = categoryName;
+//        this.categoryDescription = categoryDescription;
+//        this.status = status;
+//    }
+//
+//    public String getCategoryId() {
+//        return categoryId;
+//    }
+//
+//    public void setCategoryId(String categoryId) {
+//        this.categoryId = categoryId;
+//    }
+//
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
+//
+//    public String getCategoryDescription() {
+//        return categoryDescription;
+//    }
+//
+//    public void setCategoryDescription(String categoryDescription) {
+//        this.categoryDescription = categoryDescription;
+//    }
+//
+//    public String getActive() {
+//        return status;
+//    }
+//
+//    public void setActive(String status) {
+//        this.status = status;
+//    }
+//}
+
+
+
+/**
+ * This is version of medel using lombok
+ */
+
 package com.example.PosBit.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
-@Table(name="category")
+@Table(name = "category")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
 
     @Id
     private String categoryId;
+
     private String categoryName;
 
     private String categoryDescription;
 
     private String status;
 
-    public Category() {}
-
-    public Category(String categoryId, String categoryName, String categoryDescription, String status) {
-        super();
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-        this.categoryDescription = categoryDescription;
-        this.status = status;
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public String getCategoryDescription() {
-        return categoryDescription;
-    }
-
-    public void setCategoryDescription(String categoryDescription) {
-        this.categoryDescription = categoryDescription;
-    }
-
-    public String getActive() {
-        return status;
-    }
-
-    public void setActive(String status) {
-        this.status = status;
-    }
+    @OneToMany(mappedBy = "category")
+    @JsonIgnore
+    private List<Product> products;
 }
--- src/main/java/com/example/PosBit/model/Product.java
+++ src/main/java/com/example/PosBit/model/Product.java
@@ -1,3 +1,128 @@
+//package com.example.PosBit.model;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.Min;
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+//
+//import java.util.Date;
+//
+//@Entity
+//@Table(name="product")
+//public class Product {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer productId;
+//    @Column(nullable = false, length = 300)
+//    @NotBlank(message = "Please enter product name!")
+//    private String productName;
+//    private String productDescription;
+//    @NotNull(message = "Price is required")
+//    @Min(value = 0, message = "Price must be >= 0")
+//    private double productPrice;
+//    @Min(value = 0, message = "Quantity must be greater than 0")
+//    private Integer productQuantity = 0;
+//    @JsonProperty("isActive")
+//    private Boolean isActive = true;
+//    private Date createdDate;
+//    private Date modifiedDate;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "category_id", nullable = false)
+//    private Category category;
+//
+//
+//    public Product() {}
+//
+//    public Product(Integer productId, String productName, String productDescription, double productPrice, Integer productQuantity, Category category)
+//    {
+//        super();
+//        this.productId = productId;
+//        this.productName = productName;
+//        this.productDescription = productDescription;
+//        this.productPrice = productPrice;
+//        this.productQuantity = productQuantity;
+//        this.isActive = true;
+//        this.createdDate = new Date();
+//        this.modifiedDate = new Date();
+//        this.category = category;
+//    }
+//
+//    public Integer getProductId() {
+//        return productId;
+//    }
+//
+//    public void setProductId(Integer productId) {
+//        this.productId = productId;
+//    }
+//
+//    public String getProductName() {
+//        return productName;
+//    }
+//
+//    public void setProductName(String productName) {
+//        this.productName = productName;
+//    }
+//
+//    public String getProductDescription() {
+//        return productDescription;
+//    }
+//
+//    public void setProductDescription(String productDescription) {
+//        this.productDescription = productDescription;
+//    }
+//
+//    public double getProductPrice() {
+//        return productPrice;
+//    }
+//
+//    public void setProductPrice(double productPrice) {
+//        this.productPrice = productPrice;
+//    }
+//
+//    public Integer getProductQuantity() {
+//        return productQuantity;
+//    }
+//
+//    public void setProductQuantity(Integer productQuantity) {
+//        this.productQuantity = productQuantity;
+//    }
+//
+//    public Boolean getActive() {
+//        return isActive;
+//    }
+//
+//    public void setActive(Boolean active) {
+//        isActive = active;
+//    }
+//
+//    public Date getCreatedDate() {
+//        return createdDate;
+//    }
+//
+//    public void setCreatedDate(Date createdDate) {
+//        this.createdDate = createdDate;
+//    }
+//
+//    public Date getModifiedDate() {
+//        return modifiedDate;
+//    }
+//
+//    public void setModifiedDate(Date modifiedDate) {
+//        this.modifiedDate = modifiedDate;
+//    }
+//
+//    public Category getCategory() {
+//        return category;
+//    }
+//
+//    public void setCategory(Category category) {
+//        this.category = category;
+//    }
+//
+//}
+
 package com.example.PosBit.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -5,105 +130,45 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "category")
+@EqualsAndHashCode(exclude = "category")
 @Entity
-@Table(name="product")
+@Table(name = "product")
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer productId;
+
     @Column(nullable = false, length = 300)
     @NotBlank(message = "Please enter product name!")
     private String productName;
+
     private String productDescription;
+
     @NotNull(message = "Price is required")
     @Min(value = 0, message = "Price must be >= 0")
     private double productPrice;
-    @Min(value = 0, message = "Quantity must be greater than 0")
+
+    @Min(value = 0, message = "Quantity must be >= 0")
     private Integer productQuantity = 0;
+
     @JsonProperty("isActive")
     private Boolean isActive = true;
+
     private Date createdDate;
     private Date modifiedDate;
 
-    public Product() {}
-
-    public Product(Integer productId, String productName, String productDescription, double productPrice, Integer productQuantity)
-    {
-        super();
-        this.productId = productId;
-        this.productName = productName;
-        this.productDescription = productDescription;
-        this.productPrice = productPrice;
-        this.productQuantity = productQuantity;
-        this.isActive = true;
-        this.createdDate = new Date();
-        this.modifiedDate = new Date();
-    }
-
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public double getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(double productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public Integer getProductQuantity() {
-        return productQuantity;
-    }
-
-    public void setProductQuantity(Integer productQuantity) {
-        this.productQuantity = productQuantity;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(Date modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 }
